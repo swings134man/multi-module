fe9db75d3707f0cb28c41dbb66ffc8bf17bd9ff7
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-11-30 22:04:44 +0900

    BO) 메인페이지 - 페이지 이동 버튼 추가... 임시

--- cms/frontend/src/pages/main/main-page.vue
+++ cms/frontend/src/pages/main/main-page.vue
@@ -3,12 +3,26 @@
 
   <q-page class="row items-center justify-evenly">
 
+    <!--  임시   -->
+    <q-list v-model="list">
+      <q-btn
+        v-for="item in list"
+        :key="item.label"
+        :label="item.label"
+        color="primary"
+        @click="$router.push(item.link)">
+      </q-btn>
+    </q-list>
 
   </q-page>
 </template>
 
 
 <script setup lang="ts">
+
+const list = [
+  {label: 'test1', link: '/test1'},
+]
 
 </script>
 
