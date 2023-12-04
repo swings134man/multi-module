292892c70627cd778cd04e18e6d42649cb8fe813
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-12-04 23:25:14 +0900

    BO) CORS 설정

--- cms/backend/src/main/java/com/test/CmsApp.java
+++ cms/backend/src/main/java/com/test/CmsApp.java
@@ -2,10 +2,23 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CmsApp {
     public static void main(String[] args) {
         SpringApplication.run(CmsApp.class);
+    }
+
+    @Bean
+    public WebMvcConfigurer WebConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
