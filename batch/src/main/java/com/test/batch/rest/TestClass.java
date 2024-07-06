b09228f8a231d1367e2e92f3522c0a04edfac762
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-07-06 21:29:02 +0900

    Batch module 수정중

---
+++ batch/src/main/java/com/test/batch/rest/TestClass.java
@@ -0,0 +1,24 @@
+//package com.test.batch.rest;
+//
+//import com.test.core.modules.test.service.TestService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@Slf4j
+//public class TestClass {
+//
+//    private final TestService testService;
+//
+//    @GetMapping(value = "/{message}")
+//    public ResponseEntity<String> test(@PathVariable String message) {
+//        String result = testService.test(message);
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
+//}
